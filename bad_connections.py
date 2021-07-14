b7aeb7455907c180a8956244f4d598fa8371233e
I'm Blotnoy <78096790+SocialEnot@users.noreply.github.com>
2021-07-14 21:26:19 +0300

    Update bad_connections.py

--- bad_connections.py
+++ bad_connections.py
@@ -1,36 +1,55 @@
-# -*- coding: utf-8 -*-
-from pythonping import ping
-import platform
-import colorama
-import random
-from colorama import Fore, Back, Style
-colorama.init()
-from sys import platform
 import time
 import os
-from time import sleep
+import psutil
+import multiprocessing
+import platform
+import colorama
+from colorama import *
+colorama.init()
+from pythonping import ping
 
 root = """
 █▄▄ ▄▀█ █▀▄ █▀▀ █▀█ █▄░█ █▄░█ █▀▀ █▀▀ ▀█▀ █ █▀█ █▄░█ █▀
 █▄█ █▀█ █▄▀ █▄▄ █▄█ █░▀█ █░▀█ ██▄ █▄▄ ░█░ █ █▄█ █░▀█ ▄█"""
+
+#sys
+system = platform.uname()
+system1 = platform.platform()
+processor = platform.processor()
+
+#заряд батареи 
+battery = psutil.sensors_battery()
+percent = int(battery.percent)
+
+def main():
+	battery1 = print(f"battery: {percent}%")
+	print(system)
+	print(system1)
+	print("processor > " + processor)
+	input("press any key")
+
+
 print(root)
-print("""bad_connections true utility""")
-print("""
-команда для помощи help""")
-user_pass = input("""[bad_connections] - """)
-if user_pass == 'help':
+print("Run a system check")
+roinput = input("""[bad_connections] - """)
+if roinput == 'check_system-pcc':
+	main()
+
+if roinput == 'help':
     print("""1) all names in computer(txt) - check_user-pcc
 2) os - check_os-pcc
 3) ping - check_monitor-pcc
+4) check - check_system-pcc
 4) exit - exit!""")
-    user_pass = input("""[bad_connections] - """)
-if user_pass == 'check_user-pcc':
+    roinput = input("""[bad_connections] - """)
+
+if roinput == 'check_user-pcc':
     os.system('net user > users.txt')
     time.sleep(1.3)
     print(Fore.GREEN + 'save is good')
     print(Style.RESET_ALL)
     input("press any key")
-if user_pass == 'check_os-pcc':
+if roinput == 'check_os-pcc':
     if platform == "linux" or platform == "linux2":
         print(Fore.YELLOW + "system platform linux")
     elif platform == "darwin":
@@ -39,8 +58,8 @@ if user_pass == 'check_os-pcc':
         print(Fore.YELLOW + "system platform windows")
     print(Style.RESET_ALL)
     input("press any key")
-if user_pass == 'check_monitor-pcc':
+if roinput == 'check_monitor-pcc':
     ping('8.8.8.8', verbose=True)
     input("press any key")
-if user_pass == 'exit!':
+if roinput == 'exit!':
     input("button")
